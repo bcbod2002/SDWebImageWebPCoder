115ab7420c19a7b5a1ad1545d864baa2c84d9ac1
Goston <bcbod2002@hotmail.com.tw>
2020-09-17 10:15:57 +1000

    Update Package.swift
    Remove platform, and update SDWebImage dependency's version number

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SDWebImageWebPCoder",
-    platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -17,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.7.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.8.2"),
         .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.1.0")
     ],
     targets: [
